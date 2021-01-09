4d883b81c0c859fc9f9e0749fbb755f77fe8149f
Ernie <jasonernie03@gmail.com>
2021-01-08 22:43:55 -0500

    time serious part2

--- time serious/timeSeriesOC.py
+++ time serious/timeSeriesOC.py
@@ -18,7 +18,10 @@ Created on Wed Jan  6 10:57:38 2021
 # # ]
 # # 
 # #
-# # What we really want is to have one point on our graph for each integer X value. Notice that we don't have a coordinate where x is 2, 3, 4, 6, 7, 8, 9, etc. Let's fill in these coordinates, and set the corresponding Y values to be such that we generate straight lines between the points we know.
+# # What we really want is to have one point on our graph for each integer X value. Notice that we don't have a coordinate 
+# # where x is 2, 3, 4, 6, 7, 8, 9, etc. 
+# # Let's fill in these coordinates, and set the corresponding Y values to be such that we generate straight
+# # lines between the points we know.
 # #
 # # expected_output = [
 # #   { x: 1, y: 1.25 }, <-- same as input
@@ -42,26 +45,40 @@ Created on Wed Jan  6 10:57:38 2021
 
 timeseries = [ { 'x': 1, 'y': 1.25 }, { 'x': 5, 'y': 2.25 }, { 'x': 10, 'y': 1 }, { 'x': 20, 'y': 2.12 }]
 
-
+timeseriesToBeComplted = timeseries.copy()
 #print(f'Songs --> {songs} \n')
 #title = list(map(lambda x : x['title'], songs))
 #print(f'Print Title --> {title}')
 
 
 
-def outPutNewArray(ts): 
-    previous = next_ = None
-    lengthOfTs = len(ts)
-    for index, obj in enumerate(ts):
-        print('index is: ', index)
-        print('obj is ', obj)
+def outPutNewArray(ts, tsToBeFilled): 
+    
+    previous = None
+    # next_ = None
+    # lengthOfTs = len(ts)
+    for index, currentObj in enumerate(ts):    # access the index and the item itself
+        # print('index is: ', index)
+        # print('obj is ', currentObj)
        
-        if index > 0:
-            previous = ts[index - 1]
-            print('previous is ', previous)
-        if index < (lengthOfTs - 1):
-            next_ = ts[index + 1]
-            print('next item is', next_)
+        if index > 0:                          # access from the second element in the list
+            previous = ts[index - 1]           # get access to previous element
+            # print('previous is ', previous)
+            k =  (currentObj['y'] - previous['y']) / (currentObj['x'] - previous['x'])        
+            # print('the k is: ', k)
+            b = currentObj['y'] - k * (currentObj['x'])
+            # print('the b is: ', b)  
+            
+            for i in range(1,currentObj['x'] - previous['x']):  # filling in the gap
+                # print('序数:', previous['x'] + i)
+                yTobeFilled = k * (previous['x'] + i) + b
+                tsToBeFilled.insert(i + previous['x'] - 1, {'x': previous['x'] + i , 'y': round(yTobeFilled, 2)})  #insert before certain index                 
+            
+    
+    return tsToBeFilled
+        # if index < (lengthOfTs - 1):   # access to the next element in the for loop
+        #     next_ = ts[index + 1]
+        #     print('next item is', next_)
            
-outPutNewArray(timeseries)
-
+finalResult = outPutNewArray(timeseries, timeseriesToBeComplted)
+print(finalResult)
