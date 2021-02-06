6d953fdd462d758a9112a025e66ad01c28f889ae
Ernie <jasonernie03@gmail.com>
2021-02-06 11:32:48 -0500

    oop practice 1

--- machine learning oop practice/python oop.py
+++ machine learning oop practice/python oop.py
@@ -6,4 +6,18 @@ Created on Fri Feb  5 21:37:02 2021
 @author: ernie
 """
 
-class 
+class XuLie:
+    
+    def __init__(self, property1, property2, property3):
+        self.classProperty1 = property1
+        self.classProperty2 = property2
+        self.classProperty3 = property3    
+        
+
+
+
+xuLieInstance = XuLie('passed string', 123, 'another string')
+
+
+
+print(xuLieInstance.classProperty1)
