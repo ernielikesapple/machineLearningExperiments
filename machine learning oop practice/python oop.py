1a071769d55c117f277f5ac1a1cb2d6e0b34a570
Ernie <jasonernie03@gmail.com>
2021-02-09 11:53:54 -0500

    inheritance

--- machine learning oop practice/python oop.py
+++ machine learning oop practice/python oop.py
@@ -14,6 +14,8 @@ class XuLie:
     
     def __init__(self, property1, property2, property3):
         self.classProperty1 = property1               # this is a instance variable, when calling like Class.property, it won't show
+        
+        print("one class has been initiated, first property is: ", property1)
         self.classProperty2 = property2
         self.classProperty3 = property3  
         
@@ -35,6 +37,53 @@ class XuLie:
     def thisIsAstaticMethod(FirstParameterDontNeedToBeClassOrInstance):
         return print(FirstParameterDontNeedToBeClassOrInstance, "if you are not accessing the class or instance anywhere in the function")
         
+    
+    
+
+class DianLiGongYingXuLie(XuLie):
+    specialChildProperty = "this property is unique from the parent class"
+    
+    def __init__(self, property1, property2, property3, addtionalChildPropery1):
+        super().__init__(property1, property2, property3)
+        #XuLie.__init__(self, property1, property2, property3)   # another way to init from the parent class
+        self.addtionalChildPropery1 = addtionalChildPropery1 #this property is unique from the parent class
+        
+
+
+class ZhenKongXuLie(XuLie):
+    specialChildProperty = "this property is unique from the parent class"
+    
+    def __init__(self, property1, property2, property3, buzhouLists=None):
+        super().__init__(property1, property2, property3)
+        #XuLie.__init__(self, property1, property2, property3)   # another way to init from the parent class
+        if buzhouLists is None:  # never pass mutable data types like lists or dictionary as default arguments
+            self.buzhouLists = []  
+        else:
+            print("add buzhou into lists")
+            self.buzhouLists = buzhouLists
+        
+    def add_Buzhou(self, buzhou):
+        if buzhou not in self.buzhouLists:
+            self.buzhouLists.append(buzhou)
+    
+    def remove_Buzhou(self, buzhou):
+        if buzhou in self.buzhouLists:
+            self.buzhouLists.remove(buzhou)    
+    
+    def print_buZhou(self):
+        for buzhou in self.buzhouLists:
+            print(" 打印所有buzhou-->", buzhou.mingzi)
+        
+
+
+class Buzhou:
+    
+    def __init__(self, mingzi, miaoshu):
+        self.mingzi = mingzi
+        self.miaoshu = miaoshu
+        
+        
+
 
 xuLieInstance = XuLie('passed string', 123, 'another string')
 
@@ -58,3 +107,28 @@ newXuLieInstance = XuLie.alternativeClassInstructor("sad@aaa@bbb")
 print("we have sum up : ", XuLie.classCounter, "classes instance have been instantiated")
 
 newXuLieInstance.thisIsAstaticMethod("static method is like: ")
+
+
+dianLiGongYingXuLie = DianLiGongYingXuLie('this is an inherited class', 123, 'another string', "this property is unique from the parent class")
+
+print("we have sum up : ", XuLie.classCounter, "classes instance have been instantiated")
+print("child has a unique property ", dianLiGongYingXuLie.addtionalChildPropery1, " and it has all the parent property:", dianLiGongYingXuLie.classProperty1)
+
+
+
+buzhou1 = Buzhou("this is a buzhou", "test")
+buzhou2 = Buzhou("this is a buzhou2", "test")
+zhenkongXuLieCeShi = ZhenKongXuLie("123", "123", "123", [buzhou1])
+
+zhenkongXuLieCeShi.add_Buzhou(buzhou2)
+zhenkongXuLieCeShi.remove_Buzhou(buzhou1)
+
+zhenkongXuLieCeShi.print_buZhou()
+
+print(help(ZhenKongXuLie))  #check inheritance, in the "Method resolution order:" section
+print(issubclass(ZhenKongXuLie, XuLie))
+print(isinstance(zhenkongXuLieCeShi, XuLie))
+
+
+
+
